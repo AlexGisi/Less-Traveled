b9c4a513ee14d1dd8d8f655f58191d285a2ca940
Alex Gisi <apgisi11@gmail.com>
2020-12-14 13:45:43 -0600

    Initial Commit

---
+++ Less Traveled/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Less Traveled
+//
+//  Created by Alex Gisi on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
